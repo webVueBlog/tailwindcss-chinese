6ea303804c31ed7a5af7fe1602644a8fe2a4d466
webVueBlog <2979986590@qq.com>
2023-04-25 13:48:42 +0800

    feat: deploy

--- deploy.sh
+++ deploy.sh
@@ -10,7 +10,7 @@ git checkout -b dist
 git add -A
 git commit -m 'deploy'
 printf "本地提交成功\n"
-# git push -f git@github.com:webVueBlog/awesome-css.git dist
-git push -f https://github.com/webVueBlog/awesome-css.git dist
+git push -f git@github.com:webVueBlog/tailwindcss-chinese.git dist
+# git push -f https://github.com/webVueBlog/awesome-css.git dist
 
 printf "dist目录上传成功\n"
